8a7831bf6ffba3c675ee6c4145354cdf3f18a584
AebiM <manuel.aebi@students.fhnw.ch>
2025-11-05 14:10:01 +0100

    Update BB_Abdekung_Prozent_Gemeinde.py

--- scripts/preprocessing/av/04_Vollstaendigkeit/BB_Abdekung_Prozent_Gemeinde.py
+++ scripts/preprocessing/av/04_Vollstaendigkeit/BB_Abdekung_Prozent_Gemeinde.py
@@ -5,12 +5,11 @@ import os
 # Schritt 0: Pfad definieren
 # -------------------------------
 
-wd = os.getcwd()
-path = os.path.join(wd, '..', '..', '..','..')
+path = os.getcwd()
 path = os.path.normpath(path)  # normalize path separators
 
-gem_CH_datei = "data/swissBOUNDARIES3D_1_5_LV95_LN02.gpkg"
-BB_CH_datei = "data/preprocessing/av/BB_CH_Gesamt.gpkg"
+gem_CH_datei = r"data\swissBOUNDARIES3D_1_5_LV95_LN02.gpkg"
+BB_CH_datei = r"data\preprocessing\av\BB_CH_Gesamt.gpkg"
 
 gem_CH_dir = os.path.join(path,gem_CH_datei)
 BB_CH_dir = os.path.join(path,BB_CH_datei)
@@ -23,7 +22,7 @@ print("Pfade Definiert")
 Gem_CH = gpd.read_file(gem_CH_datei, layer = "tlm_hoheitsgebiet")
 
 BB_CH = gpd.read_file(BB_CH_dir)
-print("Layer eingelesen erfolgreich.")
+print("Layer einlesen erfolgreich.")
 
 # -------------------------------
 # Schritt 2: Eindeutige ID für Gem_CH erzeugen
@@ -37,7 +36,6 @@ print("FID für Gem_CH erfolgreich erstellt.")
 # -------------------------------
 Gem_CH["area_total"] = Gem_CH.geometry.area
 print("Flächen der Gem_CH-Polygone berechnet erfolgreich.")
-
 print("Gemeindeflächen Berechnet")
 
 # -------------------------------
@@ -45,6 +43,8 @@ print("Gemeindeflächen Berechnet")
 # -------------------------------
 
 BB_CH_Union = BB_CH.union_all()
+print("BB Flächen vereinigt erfolgreich.")
+
 
 # -------------------------------
 # Schritt 4: Overlay zur Berechnung der Schnittflächen
@@ -55,6 +55,7 @@ BB_gdf = gpd.GeoDataFrame(geometry=[BB_CH_Union], crs=Gem_CH.crs)
 
 # ausfführung Overlay
 differenz_Union = gpd.overlay(Gem_CH, BB_gdf, how="difference")
+print("Overlay zur Berechnung der Schnittflächen erfolgreich.")
 
 # -------------------------------
 # Schritt 5: Abdeckung in Prozent berechnen
@@ -62,16 +63,17 @@ differenz_Union = gpd.overlay(Gem_CH, BB_gdf, how="difference")
 
 differenz_Union["area_diff"] = differenz_Union.geometry.area
 differenz_Union["prozent_diff"] = (differenz_Union["area_diff"] / differenz_Union["area_total"]) * 100
-
+print("Abdeckung in Prozent berechnet erfolgreich.")
 
 # -------------------------------
 # Schritt 7: erstellen von Mulipolygons für fehlende Abdeckung (fehlende Flächen > 99% Abdeckung)
 # -------------------------------
 fehlende_Flaeche = differenz_Union[differenz_Union["prozent_diff"] > 99]
-
+print("Fehlende Flächen mit mehr als 99% Abdeckung extrahiert erfolgreich.")
 # -------------------------------
 # Schritt 8: Neues GeoPackage speichern
 # -------------------------------
 
 fehlende_Flaeche.to_file(
     "C:/Users/aebim/Documents/02_Ausbildung/Studium/05_Semester/5230_Geoniformatik_Raumanalyse/Projektarbeit/05_Daten/fehlende_Flaeche.gpkg", layer = "fehlende_Fla", driver="GPKG")
+print("Neues GeoPackage mit fehlenden Flächen gespeichert erfolgreich.")
