9df299f80fdf0c7eace4363fb710defc9fd3097f
JonasHeinz <jonasheinz23@gmail.com>
2025-11-25 23:40:54 +0100

    version 1 shiny app für alle Datensätze

--- scripts/visualization/as-corine.py
+++ scripts/visualization/as-corine.py
@@ -1,27 +1,27 @@
 # Start with: python -m shiny run -r scripts/visualization/as-corine.py
 
+from shinywidgets import output_widget, render_plotly, render_widget
+from shiny import App, ui, reactive
+from scripts import DATA_DIR
+from rasterio.warp import transform_bounds
+import rasterio
+from PIL import Image
+from ipywidgets import Layout
+from ipyleaflet import Map, ImageOverlay, basemaps, basemap_to_tiles
+from io import BytesIO
+import plotly.graph_objects as go
+import pandas as pd
+import numpy as np
+import geopandas as gpd
+import base64
 import os
 os.environ['PROJ_LIB'] = r"C:\Users\LeonardoS\miniconda3\envs\corine\Library\share\proj"
 
-import base64
-import geopandas as gpd
-import numpy as np
-import pandas as pd
-import plotly.graph_objects as go
-from io import BytesIO
-from ipyleaflet import Map, ImageOverlay, basemaps, basemap_to_tiles
-from ipywidgets import Layout
-from PIL import Image
-import rasterio
-from rasterio.warp import transform_bounds
-from scripts import DATA_DIR
-from shiny import App, ui, reactive
-from shinywidgets import output_widget, render_plotly, render_widget
 
 # -----------------------------
 # 1) Daten laden
 # -----------------------------
-
+ 
 # Kategorien inkl. Farben zusammenfassen
 ipcc_category_info = {
     1: {"name": "FL", "bar_ids": [0, 1], "colors": ("#006d2c", "#2ca25f")},
@@ -29,82 +29,156 @@ ipcc_category_info = {
     3: {"name": "Dritte", "bar_ids": [4, 5], "colors": ("#d9f0a3", "#ffffcc")},
     4: {"name": "Vierte", "bar_ids": [6, 7], "colors": ("#253494", "#2c7fb8")},
     5: {"name": "Fünfte", "bar_ids": [8, 9], "colors": ("#bdbdbd", "#d9d9d9")},
-    6: {"name": "Sechste", "bar_ids": [10,11], "colors": ("#f768a1", "#fa9fb5")},
+    6: {"name": "Sechste", "bar_ids": [10, 11], "colors": ("#f768a1", "#fa9fb5")},
 }
 
-csv_path = DATA_DIR / "analysis/corine/2018/stats_center_point.csv"
 
-if not csv_path.exists():
-    gdf = gpd.read_file(DATA_DIR / "analysis/corine/2018/result_center_point.gpkg") 
-    gdf = gdf.to_crs(epsg=3857)
-    print("Initial Data loaded from Geopackage")
+files = [
+    {"dataset": "corine", "year": "2012", "analysis": "result_center_point", "output": "center_point_raster", "as_id": "IPCC_AS_Id", "match": "IPCC_Match"},
+    {"dataset": "corine", "year": "2012", "analysis": "result_max_area", "output": "max_area_raster", "as_id": "IPCC_AS_Id", "match": "IPCC_Match"},
+    {"dataset": "corine", "year": "2018", "analysis": "result_center_point", "output": "center_point_raster", "as_id": "IPCC_AS_Id", "match": "IPCC_Match"},
+    {"dataset": "corine", "year": "2018", "analysis": "result_max_area", "output": "max_area_raster", "as_id": "IPCC_AS_Id", "match": "IPCC_Match"},
+    
+    {"dataset": "worldcover", "year": "2020", "analysis": "arealstatistik_mapped_points_2020_cp", "output": "center_point_raster", "as_id": "IPCC_AS_Id", "match": "IPCC_Match"},
+    {"dataset": "worldcover", "year": "2020", "analysis": "arealstatistik_mapped_2020_ma", "output": "max_area_raster", "as_id": "IPCC_AS_Id", "match": "IPCC_Match"},
+    {"dataset": "worldcover", "year": "2021", "analysis": "arealstatistik_mapped_points_2021_cp", "output": "center_point_raster", "as_id": "IPCC_AS_Id", "match": "IPCC_Match"},
+    {"dataset": "worldcover", "year": "2021", "analysis": "arealstatistik_mapped_2021_ma", "output": "max_area_raster", "as_id": "IPCC_AS_Id", "match": "IPCC_Match"},
+    
+    #{"dataset": "av", "year": "jahr", "analysis": "AV_As_Center_Pixel", "output": "center_point_raster", "as_id": "IPCC_AS_Id", "match": "IPCC_Match"}
+]
 
 
-    df_balken_list = []
+# Verfügbare Jahre pro Datensatz für Select-Input
+dataset_years = {
+    "corine":[2012, 2018],
+    "worldcover": [2020, 2021],
+    #"av": [2025]
+}
 
-    for ipcc_as_id in sorted(gdf["IPCC_AS_Id"].unique()):
-        subset = gdf[gdf["IPCC_AS_Id"] == ipcc_as_id]
-        cat_info = ipcc_category_info.get(ipcc_as_id)
-        name = cat_info["name"]
-        dark, light = cat_info["colors"]
+def get_tif_paths(dataset, year, method):
+    base = DATA_DIR / f"analysis/{dataset}/{year}/{method}_raster"
+    return [
+        base / f"ipcc_category_{i}.tif"
+        for i in range(1, 7)
+    ]
+for file in files:
+ 
+    path = DATA_DIR / f"analysis/{file['dataset']}/{file['year']}/{file['output']}"
+    csv_path = path / "ipcc_category_stats.csv"
+    as_id_col = file["as_id"]
+    match_col = file["match"]
+    analysis_col = file["analysis"]
+    if not csv_path.exists():
+        gdf = gpd.read_file(
+            path / f"{analysis_col}.gpkg")
+        gdf = gdf.to_crs(epsg=3857)
+        print(f"Initial Data loaded from Geopackage from {analysis_col}.gpkg")
+        if file["dataset"] == "worldcover":
+                gdf["IPCC_Match"] = gdf["IPCC_AS_Id"] == gdf["IPCC_WC_Id"]
+        df_balken_list = []
 
-        df_temp = pd.DataFrame({
-            "IPCC_AS_Id": [ipcc_as_id, ipcc_as_id],
-            "IPCC_AS_Name": [name, name],
-            "Art": ["AS", "AS"],
-            "Uebereinstimmende_Klassifikation": ["Wahr", "Falsch"],
-            "Farbe": [light, dark],
-            "Anzahl": [subset["IPCC_Match"].sum(), len(subset) - subset["IPCC_Match"].sum()],
-            "Anteil": [subset["IPCC_Match"].mean(), 1 - subset["IPCC_Match"].mean()]
-        })
-        df_balken_list.append(df_temp)
+        for ipcc_as_id in sorted(gdf[as_id_col].unique()):
+            subset = gdf[gdf[as_id_col] == ipcc_as_id]
+            cat_info = ipcc_category_info.get(ipcc_as_id)
+            name = cat_info["name"]
+            dark, light = cat_info["colors"]
 
-    df_balken = pd.concat(df_balken_list, ignore_index=True)
-    print("Stats calculated from Geopackage")
-    df_balken.to_csv(csv_path, index=False)
-    print(f"Stats saved to CSV")
+            df_temp = pd.DataFrame({
+                "IPCC_AS_Id": [ipcc_as_id, ipcc_as_id],
+                "IPCC_AS_Name": [name, name],
+                "Art": ["AS", "AS"],
+                "Uebereinstimmende_Klassifikation": ["Wahr", "Falsch"],
+                "Farbe": [light, dark],
+                "Anzahl": [subset[match_col].sum(), len(subset) - subset[match_col].sum()],
+                "Anteil": [subset[match_col].mean(), 1 - subset[match_col].mean()]
+            })
+            df_balken_list.append(df_temp)
 
-else:
-    df_balken = pd.read_csv(csv_path)
-    print("Stats loaded from CSV")
+        df_balken = pd.concat(df_balken_list, ignore_index=True)
+        print("Stats calculated from Geopackage")
+        df_balken.to_csv(csv_path, index=False)
+        print(f"Stats saved to CSV")
+    else:
+        print(f"Stats CSV already exists at {csv_path}, skipping processing.")
 
 # -----------------------------
 # 2) UI
 # -----------------------------
+
 app_ui = ui.page_fillable(
+    ui.card(
+        ui.layout_columns(
+            ui.input_select(
+                id="select_dataset",
+                label="Wähle einen Datensatz:",
+                choices={"corine": "CORINE Land Cover",
+                         "worldcover": "ESA WorldCover", "av": "Amtliche Vermessung"},
+
+            ),
+            ui.input_select(
+                id="select_year",
+                label="Wähle das Jahr:",
+                choices=[],
+            ),
+            ui.input_select(
+                id="select_method",
+                label="Wähle die Methode:",
+                choices={"center_point": "Center Point",
+                         "max_area": "Max Area"},
+            ),
+        )
+    ),
+
     ui.layout_columns(
         ui.card(output_widget("karte")),
         ui.card(output_widget("balken")),
-        col_widths=(8,4),
-    ),
+        col_widths=(8, 4),
+    )
 )
 
 # -----------------------------
 # 3) Server
 # -----------------------------
+
+
 def server(input, output, session):
 
     all_layers = {}
-    
+
     category_display = {
         cat_id: {"true": reactive.Value(False), "false": reactive.Value(False)}
         for cat_id in ipcc_category_info.keys()
     }
-    
-    #Karteneinstellungen
+
+    # Karteneinstellungen
     m = Map(
-        center=(46.8, 8.3), #Kartenzentrum
-        zoom=8, #Zoomstufe
-        layout=Layout(width='100%', height='80vh') # Layout der Kartenbox
+        center=(46.8, 8.3),  # Kartenzentrum
+        zoom=8,  # Zoomstufe
+        layout=Layout(width='100%', height='80vh')  # Layout der Kartenbox
     )
     m.add(basemap_to_tiles(basemaps.CartoDB.Positron))
-    
+
     # Balkendiagramm
     @output
     @render_plotly
     def balken():
+        # CSV dynamisch laden
+        dataset = input.select_dataset()
+        year = input.select_year()
+        method = input.select_method()
+
+        if not dataset or not year or not method:
+            return pd.DataFrame()
+
+        csv_path = (
+            DATA_DIR /
+            f"analysis/{dataset}/{year}/{method}_raster/ipcc_category_stats.csv"
+        )
+        df_balken = pd.read_csv(csv_path)
+
+        # Balkendiagramm dynamisch darstellen
         fig = go.Figure([go.Bar(
-            x=df_balken["Anteil"], 
+            x=df_balken["Anteil"],
             y=df_balken["IPCC_AS_Name"],
             orientation='h',
             marker_color=df_balken["Farbe"]
@@ -115,7 +189,7 @@ def server(input, output, session):
         w = go.FigureWidget(fig)
         w.data[0].on_click(on_point_click)
         return w
-    
+
     def on_point_click(trace, points, state):
         idx = points.point_inds[0]
         for ipcc_category, cat_info in ipcc_category_info.items():
@@ -134,14 +208,17 @@ def server(input, output, session):
                 else:
                     m.add(layer0)
                     category_display[ipcc_category]["false"].set(True)
-    
+
     @output
     @render_widget
     def karte():
-        tif_paths = [
-            DATA_DIR / f"analysis/corine/2018/center_point_raster/ipcc_category_{i}.tif"
-            for i in range(1,7)
-        ]
+        # Raster dynamisch laden
+        dataset = input.select_dataset()
+        year = input.select_year()
+        method = input.select_method()
+        # Karte darstellen
+        tif_paths = get_tif_paths(dataset, year, method)
+
         for ipcc_as_id, path in zip(ipcc_category_info.keys(), tif_paths):
             colors = ipcc_category_info[ipcc_as_id]["colors"]
             layer0 = tif_to_png_overlay_value(path, colors, value_to_show=0)
@@ -151,6 +228,20 @@ def server(input, output, session):
             all_layers[ipcc_as_id] = (layer0, layer1)
         return m
 
+    @reactive.Effect
+    @reactive.event(input.select_dataset)
+    def update_years():
+        selected = input.select_dataset()
+        years = dataset_years.get(selected, [])
+
+        # Jahre im Dropdown ersetzen
+        ui.update_select(
+            "select_year",
+            choices=years,
+            selected=years[0] if years else None
+        )
+
+
 # -----------------------------
 # 4) App starten
 # -----------------------------
@@ -159,14 +250,17 @@ app = App(app_ui, server)
 # -----------------------------
 # Overlay-Funktion
 # -----------------------------
+
+
 def tif_to_png_overlay_value(path, colors, value_to_show=255, upscale=2):
     with rasterio.open(path) as src:
         arr = src.read(1)
-        minx, miny, maxx, maxy = transform_bounds(src.crs, "EPSG:4326", *src.bounds)
+        minx, miny, maxx, maxy = transform_bounds(
+            src.crs, "EPSG:4326", *src.bounds)
 
     img = np.zeros((arr.shape[0], arr.shape[1], 4), dtype=np.uint8)
-    dark  = tuple(int(colors[0].lstrip("#")[i:i+2], 16) for i in (0,2,4))
-    light = tuple(int(colors[1].lstrip("#")[i:i+2], 16) for i in (0,2,4))
+    dark = tuple(int(colors[0].lstrip("#")[i:i+2], 16) for i in (0, 2, 4))
+    light = tuple(int(colors[1].lstrip("#")[i:i+2], 16) for i in (0, 2, 4))
 
     img[arr == 0, :3] = dark
     img[arr == 1, :3] = light
@@ -179,7 +273,8 @@ def tif_to_png_overlay_value(path, colors, value_to_show=255, upscale=2):
         img[arr == 0, 3] = 0
 
     pil = Image.fromarray(img, mode="RGBA")
-    pil = pil.resize((arr.shape[1]*upscale, arr.shape[0]*upscale), Image.NEAREST)
+    pil = pil.resize(
+        (arr.shape[1]*upscale, arr.shape[0]*upscale), Image.NEAREST)
     buffer = BytesIO()
     pil.save(buffer, format="PNG")
     encoded = base64.b64encode(buffer.getvalue()).decode()
